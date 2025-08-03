4a0863c9937122cd3ef1c4354e1af94af3508a4f
lucasamtaylor01 <lucasamtaylor@usp.br>
2025-08-03 18:00:40 -0300

    ♻️ Revisando código

--- 04_EXPLORACOES_EM_JULIA/01_PRIMEIRA_EXPLORACAO/deterministico.jl
+++ 04_EXPLORACOES_EM_JULIA/01_PRIMEIRA_EXPLORACAO/deterministico.jl
@@ -1,55 +1,24 @@
-# Sistema original
-using DifferentialEquations, Plots, Statistics, DSP
+using DifferentialEquations, ModelingToolkit, Plots, BenchmarkTools
 
-function f(du, u, p, t)
-    lambda = 1.0
-    epsilon = 0.01
+@independent_variables t
+@variables x(t)=0.1 y1(t)=0.01 y2(t)=0.01 y3(t)=0.01
+@parameters ϵ=0.01 λ=1.0
+D = Differential(t)
 
-    x, y1, y2, y3 = u
+eqs = [
+    D(x) ~  x - x^3 + (λ/ϵ) * y2,
+    D(y1) ~ (10 / ϵ^2) * (y2 - y1),
+    D(y2) ~ (1 / ϵ^2) * (28*y1 - y2 - y1*y3),  
+    D(y3) ~ (1 / ϵ^2) * (y1*y2 - (8/3)*y3)
+]
 
-    du[1] = x - x^3 + (lambda/epsilon) * y2
-    du[2] = (10 / epsilon^2) * (y2 - y1)
-    du[3] = (1 / epsilon^2) * (28*y1 - y2 - y1*y3)  
-    du[4] = (1 / epsilon^2) * (y1*y2 - (8/3)*y3)
-end
+@mtkbuild sys = ODESystem(eqs, t)
 
-u0 = [0.1, 0.01, 0.01, 0.01]
-tspan = (0.0, 100.0)
-prob = ODEProblem(f, u0, tspan)
-solucao_deterministico = solve(prob, Rodas5())
+tspan = (0.0, 1.0)
+prob = ODEProblem(sys, [], tspan)
+solucao_deterministico = solve(prob, Tsit5(), reltol=1e-6)
 
-plot(solucao_deterministico.t, solucao_deterministico[1, :];
-     label = "x(t)",
-     xlabel = "Tempo",
-     ylabel = "x",
-     title = "Modelo acoplado determinado",
-     size = (960, 540),
-     color = :red,
-     linewidth = 1.5)
-
-
-y2 = solucao_deterministico[3, :]
-y2 = y2 .- mean(y2)                    
-
-# Autocorrelação normalizada (com ρ(0) = 1)
-acor = xcorr(y2; scaling = :coeff)
-
-# Lags positivos: o centro da autocorrelação está na posição `n`
-n = 200
-lag0 = n
-acor_pos = acor[lag0:end]
-
-# Variância e passo temporal
-var_y2 = var(y2)
-dt = solucao_deterministico.t[2] - solucao_deterministico.t[1]
-
-# Parâmetro do sistema
-lambda = 1.0  
-
-# Estimativa de sigma² (com integral da função de autocorrelação)
-sigma2 = 2 * lambda^2 * var_y2 * dt * sum(acor_pos)
-sigma = sqrt(sigma2)
-
-# Exibe os resultados
-println("Estimativa de sigma² ≈ ", sigma2)
-println("Estimativa de sigma  ≈ ", sigma)
+# Centraliza y2
+y2_vals = solucao_deterministico[3, :]
+y2_centered = y2_vals .- mean(y2_vals)
+plot(solucao_deterministico.t, y2_vals)
--- 04_EXPLORACOES_EM_JULIA/01_PRIMEIRA_EXPLORACAO/estocastico.jl
+++ 04_EXPLORACOES_EM_JULIA/01_PRIMEIRA_EXPLORACAO/estocastico.jl
@@ -2,9 +2,9 @@
 
 using DifferentialEquations, Plots
 
-sigma = 23
+sigma = 0.9
 x0 = 0.1
-tspan = (0.0, 100.0)
+tspan = (0.0, 1.0)
 
 f1(X, p, t) = X - X^3
 f2(X, p, t) = sigma
